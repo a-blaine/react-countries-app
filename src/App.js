fa2dad0823a763b03b6b9ada5e5c10c43dcb2978
a-blaine <ai.blaine4@protonmail.com>
2023-06-16 16:26:23 +0100

    Created basic structure and Country component

--- src/App.js
+++ src/App.js
@@ -3,7 +3,21 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <div className="navbar">
+        <h1>Where in the world?</h1>
+      </div>
+      <form>
+        <input type="search" placeholder="Search for a country..." />
+        <select>
+          <option value="">Filter by Region</option>
+          <option value="africa">Africa</option>
+          <option value="america">America</option>
+          <option value="asia">Asia</option>
+          <option value="europe">Europe</option>
+          <option value="oceania">Oceania</option>
+        </select>
+      </form>
+      <Country />
     </div>
   );
 }
---
+++ src/Country.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function Country() {
+  return (
+    <div className="Country">
+      <div className="card">
+        <img src="/" alt="Country's flag" />
+        <div className="card-contents">
+          <h3>Germany</h3>
+          <ul>
+            <li>Population: 81,000,000</li>
+            <li>Region: Europe</li>
+            <li>Capital: Berlin</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
